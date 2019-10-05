a73541854c86e4f8c057f5c5ad14cdadf2df41d9
sammurira <sammurira@gmail.com>
2019-10-05 22:04:17 +0300

    First Commit for CRUDApp

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CrudApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.dgvemployees = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvemployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Location = new System.Drawing.Point(113, 22);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.Size = new System.Drawing.Size(217, 20);
+            this.txtfirstname.TabIndex = 1;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Location = new System.Drawing.Point(113, 54);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.Size = new System.Drawing.Size(217, 20);
+            this.txtlastname.TabIndex = 2;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.Location = new System.Drawing.Point(113, 84);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(217, 88);
+            this.txtaddress.TabIndex = 3;
+            this.txtaddress.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(113, 196);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 6;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.Btnadd_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(194, 196);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 7;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.Btnsave_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(275, 196);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 8;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.Btndelete_Click);
+            // 
+            // dgvemployees
+            // 
+            this.dgvemployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvemployees.Location = new System.Drawing.Point(368, 12);
+            this.dgvemployees.Name = "dgvemployees";
+            this.dgvemployees.Size = new System.Drawing.Size(275, 207);
+            this.dgvemployees.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvemployees);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.txtfirstname);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CRUD App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvemployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.RichTextBox txtaddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.DataGridView dgvemployees;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+using Npgsql;
+
+namespace CrudApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindingSource bs1 = new BindingSource();
+            try
+            {
+                dgvemployees.AutoGenerateColumns = true;
+
+                string sql = "select first_name, last_name, address from employee;";
+                using (var connection = new NpgsqlConnection("host=localhost;Username=postgres;Password=pass;Database=cruddb"))
+                {
+                    connection.Open();
+                    var employees = connection.Query(sql).ToList();
+                    bs1.DataSource = employees;
+                    dgvemployees.DataSource = bs1;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void Btnsave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "insert into employee (first_name,last_name, address) values (@first,@last,@address);";
+                using (var connection = new NpgsqlConnection("host=localhost;Username=postgres;Password=pass;Database=cruddb"))
+                {
+                    connection.Open();
+                    var affectedrows = connection.Execute(sql, new {
+                        first = txtfirstname.Text,
+                        last = txtlastname.Text,
+                        address = txtaddress.Text
+                    });
+                    string outputstr = affectedrows.ToString();
+                    MessageBox.Show(outputstr, "CRUD App", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+
+        
+        }
+
+        private void Btnadd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtfirstname.Text = "";
+                txtlastname.Text = "";
+                txtaddress.Text = "";   
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void Btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = "delete from  employee where first_name = @first;";
+                using (var connection = new NpgsqlConnection("host=localhost;Username=postgres;Password=pass;Database=cruddb"))
+                {
+                    connection.Open();
+                    var affectedrows = connection.Execute(sql, new
+                    {
+                        first = txtfirstname.Text
+                    });
+                    string outputstr = affectedrows.ToString();
+                    MessageBox.Show(outputstr, "coder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtfirstname.Text = "";
+                    txtlastname.Text = "";
+                    txtaddress.Text = "";
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+    }
+}
